ab9b611bedfeda8e80876d3dba029d16f68ca42e
lx466381029 <466381029@qq.com>
2025-06-19 22:09:17 +0800

    Add config/constants.py - 游戏常量定义

---
+++ config/constants.py
@@ -0,0 +1,18 @@
+# 游戏常量定义
+# Game Constants Definition
+
+# 手牌相关
+MAX_HAND_SIZE = 5           # 最大手牌数量
+INITIAL_HAND_SIZE = 5       # 初始手牌数量
+
+# 罗盘相关
+COMPASS_POSITIONS = 12      # 罗盘总位置数
+
+# 玩家初始属性
+INITIAL_PLAYER_HP = 100     # 玩家初始血量
+INITIAL_PLAYER_MP = 15      # 玩家初始魔法值
+INITIAL_PLAYER_ATK = 1      # 玩家基础攻击力（每回合重置为1）
+
+# 敌人初始属性
+INITIAL_ENEMY_HP = 80       # 敌人初始血量
+INITIAL_ENEMY_ATK = 12      # 敌人攻击力 
